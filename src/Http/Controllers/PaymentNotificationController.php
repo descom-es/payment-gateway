865c9f5fc18a69b405fc3dd065d69c7fb379336b
Cesar Garcia <cesargb@gmail.com>
2023-01-17 11:41:48 +0100

    change transition_id with merchant_id (#5)

--- src/Http/Controllers/PaymentNotificationController.php
+++ src/Http/Controllers/PaymentNotificationController.php
@@ -2,6 +2,8 @@
 
 namespace Descom\Payment\Http\Controllers;
 
+use Descom\Payment\Models\TransactionModel;
+use Descom\Payment\Payment;
 use Descom\Payment\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -10,7 +12,14 @@ class PaymentNotificationController extends Controller
 {
     public function __invoke(Request $request, string $payment_key)
     {
-        $transactionId = (int)$request->input('transaction_id');
+        $merchantId = $request->input('transaction_id');
+        $paymentId = Payment::find($payment_key)->paymentModel->id;
+
+
+        $transactionId = TransactionModel::where('merchant_id', $merchantId)
+            ->where('payment_id', $paymentId)
+            ->firstOrFail()
+            ->id;
 
         Transaction::find($transactionId)->notifyPurchase($request->all());
 
