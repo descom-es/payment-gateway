1a97de6039c59cc3886bd01ce948087f1ea30c5b
Juan José Llorens <llorensjj@gmail.com>
2023-01-31 15:53:39 +0100

    Fix: Relation

--- src/Models/HasTransaction.php
+++ src/Models/HasTransaction.php
@@ -2,9 +2,11 @@
 
 namespace Descom\Payment\Models;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
 trait HasTransaction
 {
-    public function transactions(): HasMany
+    public function transactions(): MorphMany
     {
         return $this->morphMany(TransactionModel::class, 'model');
     }
